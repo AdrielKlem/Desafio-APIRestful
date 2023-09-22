8da249f9c29eaac9a3f1158b094f09879191aed0
Adriel Klem <adrielklem16@gmail.com>
2023-09-21 21:18:58 -0300

    update numberstar to rating

--- web/src/components/Note/index.jsx
+++ web/src/components/Note/index.jsx
@@ -6,8 +6,8 @@ export function Note({ data, ...rest }) {
     return(
         <Container {...rest}>
             <h1>{data.title}</h1>
-            <Star numberstar={data.numberstar} />
-            <p>{data.desc}</p>
+            <Star rating={data.rating} />
+            <p>{data.description}</p>
             {
                 data.tags &&
                 <footer>
